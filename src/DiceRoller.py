a6b52d43f7498ef50a1ebee9aa33253979c75c99
Charles Schaefer <charlesschaefer@gmail.com>
2022-02-08 22:12:55 -0300

    Updating DiceRoller to compute the min/max values correctly.

--- src/DiceRoller.py
+++ src/DiceRoller.py
@@ -15,23 +15,35 @@ class DiceRoller:
     # @param line A single line, represented by a list of ParsedChunk objects
     # @return dict with the keys roll, min and max (all integers)
     def roll(self, line):
-        dice = []
+        min_value = 0
+        max_value = 0
         final_roll = 0
-        next_operation = "+"
+        current_operation = "+"
+        previous_operation = "+"
+
         for chunk in line:
             if chunk.value > 0:
-                final_roll = self._operate(final_roll, chunk.value, next_operation)
-                # Todo: the integer value should be considered for max/min values?
-                dice.append(chunk.value)
+                final_roll = self._operate(final_roll, chunk.value, current_operation)
+                min_value = self._operate(min_value, chunk.value, current_operation)
+                max_value = self._operate(max_value, chunk.value, current_operation)
             else:
                 for i in range(chunk.qtd):
                     die = Die(chunk.sides)
-                    dice.append(die.roll())
-                    final_roll = self._operate(final_roll, die, next_operation)
+                    die.roll()
+                    final_roll = self._operate(final_roll, die, current_operation)
+                    min_value, max_value = self._calculate_die_min_max(
+                        die, 
+                        current_operation, 
+                        min_value, 
+                        max_value
+                    )
+
+            previous_operation = current_operation
+            current_operation = chunk.operation
+            
                 
-                next_operation = chunk.operation
         
-        return RollResult(final_roll, min(dice), max(dice))
+        return RollResult(final_roll, min_value, max_value)
 
     ## Rolls the dice of all the lines
     def roll_all(self):
@@ -41,6 +53,16 @@ class DiceRoller:
         
         return result
 
+    ## Help method to calculate min/max possible of a die
+    def _calculate_die_min_max(self, die, operator, current_min, current_max):
+        if operator == "+":
+            current_min += 1
+            current_max += die.sides
+        else:
+            current_min -= die.sides
+            current_max -= 1
+        
+        return current_min, current_max
 
     ## help method to sum/subtract
     def _operate(self, value1, value2, operator):
